bd075f9a01eb9dbf74aff1e607702371eb8ee5a0
Solo <costudiolabor@gmail.com>
2021-10-26 21:30:48 +0300

    Тест UI

---
+++ Assets/Scripts/PointUseItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointUseItem : MonoBehaviour
+{
+    /// <summary>
+    /// Класс вешается на префаб точки использования вещей
+    /// </summary>
+    [Header("Name Point Use Item")]
+    public string namePointUseItem;
+
+    [Header("ID Point Use Item")]
+    public int id;
+}
