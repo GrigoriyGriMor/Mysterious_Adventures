3ba7197186c6688686b229bb52494a85365536be
rstone <romankiy@mail.ru>
2021-10-29 18:02:51 +0300

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,35 +27,39 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        spriteToTurn = transform.GetChild(0).gameObject;
-        jumpButton.onClick.AddListener(Debg);
+        _rigidbody = GetComponent<Rigidbody2D>();//получаем Rigidbody с героя, чтобы в дальнейшем можно было двигать его
+        spriteToTurn = transform.GetChild(0).gameObject;//получаем ссылку на спрайт, который будем вращать
+        jumpButton.onClick.AddListener(Debg);//подключаем к UI-кнопке функцию прыжка
     }
 
-    // Start is called before the first frame update
     private void FixedUpdate()
     {
-        //Debug.DrawRay(transform.position, Vector2.down * distance, Color.black);
-        _rigidbody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y);
-        IsGrounded = IsOnGround();
+        _rigidbody.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _rigidbody.velocity.y);//меняем направление скорости тела героя с помощью получения информации о положении ручки джойстика на экране
+        IsGrounded = IsOnGround();//проверяем, приземлился ли персонаж в текущий момент игры
 
+
+        //если джойстик отклоняется влево, поворачиваем спрайт персонажа влево
         if (_joystick.Horizontal < 0 )
         {
             spriteToTurn.transform.rotation = Quaternion.Euler(new Vector2(0,180));
         }
-        else if(_joystick.Horizontal > 0)
+       
+        //если джойстик отклоняется впрапо, поворачиваем спрайт персонажа вправо
+        else if (_joystick.Horizontal > 0)
         {
-
             spriteToTurn.transform.rotation = Quaternion.Euler(new Vector2(0, 0));
         }
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debg();
-        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    Debg();
+        //}
     }
 
-
+    /// <summary>
+    /// Проверка нахождения персонажа на земле.
+    /// </summary>
+    /// <returns></returns>
     private bool IsOnGround()
     {
         RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, distance);
@@ -70,26 +74,29 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Выполнение прыжка с заданной высотой и задержкой между попытками. Параметры меняются в инспекторе
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Jump()
     {
-        if (cooldown == false)
+        if(cooldown == false)
         {
-           
-            Debug.Log("СЕРДЦЕ ГОВНОКОДА");
+            _rigidbody.velocity = new Vector2(0, _jumpSpeed * 10);
         }
         cooldown = true;
         yield return new WaitForSeconds(jumpCooldown);
         cooldown = false;
     }
 
-
+    /// <summary>
+    /// Главная функция прыжка. 
+    /// </summary>
     public void Debg()
     {
         if (IsGrounded && cooldown == false)
         {
-
             StartCoroutine("Jump");
-            _rigidbody.velocity = new Vector2(0, _jumpSpeed * 10);
         }
     }
 
